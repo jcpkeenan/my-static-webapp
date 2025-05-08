b8cacd62da19ced3d8693c43b3f87223a911f23e
Jose Montanez <josemontanez1992@gmail.com>
2025-05-08 18:21:03 -0500

    Mobile Layout fixed

---
+++ src/JS/ourmission.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Fix for navbar toggler on mobile
+    $('.navbar-toggler').on('click', function() {
+        $('#navbarSupportedContent').toggleClass('show');
+    });
+    
+    
+});
